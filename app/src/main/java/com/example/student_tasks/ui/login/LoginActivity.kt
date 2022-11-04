fca8a5ee5516fbe79a72d7a84ff06e6e1e60d994
Alexander <inimitable73@gmail.com>
2022-11-04 21:47:45 +0400

    still ui

--- app/src/main/java/com/example/student_tasks/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/student_tasks/ui/login/LoginActivity.kt
@@ -20,5 +20,6 @@ class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login);
     }
 }
