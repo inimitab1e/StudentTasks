02642a00826ad1bbfdff25aa21fd19d62987c94e
Alexander <inimitable73@gmail.com>
2022-11-06 16:20:57 +0400

    register + login ui done
    both activities need make them fragments

--- app/src/main/java/com/example/student_tasks/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/student_tasks/ui/login/LoginActivity.kt
@@ -1,25 +1,26 @@
 package com.example.student_tasks.ui.login
 
-import android.app.Activity
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
-import android.os.Bundle
-import androidx.annotation.StringRes
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
-import android.text.Editable
-import android.text.TextWatcher
-import android.view.View
-import android.view.inputmethod.EditorInfo
-import android.widget.EditText
-import android.widget.Toast
+import android.os.Bundle
 import com.example.student_tasks.databinding.ActivityLoginBinding
-
-import com.example.student_tasks.R
+import com.example.student_tasks.ui.register.RegisterActivity
 
 class LoginActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login);
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.apply {
+            textCreateAccLink.setOnClickListener {
+                val intent = Intent(this@LoginActivity, RegisterActivity::class.java)
+                startActivity(intent)
+            }
+        }
     }
 }
