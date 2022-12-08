f42f4071f0def8d8cb0f2791ed6c4fa76e7a6106
Aleksandr <inimitable73@gmail.com>
2022-12-08 17:28:54 +0400

    login frag fixed, start with profile frag

---
+++ app/src/main/java/com/example/student_tasks/data/model/ProfileFragment.kt
@@ -0,0 +1,23 @@
+package com.example.student_tasks.data.model
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.student_tasks.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class ProfileFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
